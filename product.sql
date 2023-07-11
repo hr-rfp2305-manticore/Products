e6cac4b029b5f403fc0e8d222de865f3a791d5d2
Andy Moc <andymoc.com@gmail.com>
2023-07-11 16:09:31 -0700

    Finished with sql dbms code implementation

--- product.sql
+++ product.sql
@@ -1,35 +1,29 @@
 CREATE TABLE productlist (
-  proudct_id: INT UNSIGNED NOT NULL,
-  campus: VARCHAR NOT NULL,
+  product_id: INT UNSIGNED NOT NULL,
   product_name: VARCHAR NOT NULL,
-  product_slogan: TEXT NOT NULL,
-  product_desc: TEXT NOT NULL,
-  product_cat: VARCHAR NOT NULL,
-  product_price: DECIMAL NOT NULL,
-  product_created: DEFAULT CURRENT_TIMESTAMP,
-  product_updated: DEFAULT CURRENT_TIMESTAMP,
+  product_cat: VARCHAR NOT NULL
 )
-CREATE TABLE productinfo (
-  proudct_id: INT UNSIGNED NOT NULL,
+CREATE TABLE productitem (
+  product_id: INT UNSIGNED NOT NULL,
   product_name: VARCHAR NOT NULL,
   product_slogan: TEXT NOT NULL,
   product_desc: TEXT NOT NULL,
   product_cat: VARCHAR NOT NULL,
-  product_price: DECIMAL NOT NULL,
   product_feature: VARCHAR NOT NULL,
 )
 CREATE TABLE productstyle (
-  proudct_id: INT UNSIGNED NOT NULL,
+  product_id: INT UNSIGNED NOT NULL,
   style_id: INT UNSIGNED NOT NULL,
   style_name: VARCHAR NOT NULL,
   style_price: INT UNSIGNED NOT NULL,
   style_sale: INT UNSIGNED NOT NULL,
   style_default: BOOLEAN,
   style_photo: VARBINARY(MAX),
+  style_sku: INT UNSIGNED NOT NULL
 
 )
-CREATE TABLE cart (
-  proudct_id: INT UNSIGNED NOT NULL,
-  product_sku: INT UNSIGNED NOT NULL,
-  product_count: INT UNSIGNED NOT NULL,
+CREATE TABLE relatedProductList (
+  product_id: INT UNSIGNED NOT NULL,
+  relatedProduct_id: INT UNSIGNED NOT NULL,
+  product_cat: VARCHAR NOT NULL
 )
