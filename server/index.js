1e619f5957096ba106c4f27b1fd7ec3ca665f03e
Andy Moc <andymoc.com@gmail.com>
2023-07-14 01:01:55 -0700

    Initiating ETL Process

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = require('./route.js')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express();
+
+app.use(express.static('client/dist'));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(cors());
+app.use(express.json());
+
+app.listen(3000);
+console.log('Server listening at port 3000')
+
+module.exports.app = app
